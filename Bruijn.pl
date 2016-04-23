73711ef9acbeeb7a9bfd2d75bf252fefc650fd5a
GlassGhost <GlassGhost@users.noreply.github.com>
2016-04-23 11:33:35 -0500

    Update Bruijn.pl

--- Bruijn.pl
+++ Bruijn.pl
@@ -4,23 +4,18 @@ use Time::HiRes qw( usleep gettimeofday tv_interval stat time alarm sleep );
 use warnings;
 use IO::File;
 use Cwd; my $originalCwd = getcwd()."/";
-#primes.blc as argument for test conversion
 #______________________________________________________________________open file
-my ($name) = @ARGV;
-$FILE = new IO::File;
-#$FILE->open("< ".$originalCwd."unparsed.txt") || die("Could not open file!");
-$FILE->open("< ".$name) || die("Could not open file!");
-while (<$FILE>){ $field .= $_; }
-$FILE->close;
+my ($FName) = @ARGV;
+$FILE = new IO::File;$FILE->open("< ".$FName) || die("Could not open file!");
+while (<$FILE>){ $field .= $_; } $FILE->close;
 #______________________________________________________________________Translate
 $field =~ s/(00|01|(1+0))/$1 /gsm;
 $field =~ s/00 /\\ /gsm;
 $field =~ s/01 /(a /gsm;
 $field =~ s/(1+)0 /length($1)." "/gsme;
 
-#$RecursiveRegex = qr/([ |\\]*((\((?:RecursiveRegex)\))|\d+)[ |\\]*){2}/
-#$field =~  s/\(a${RecursiveRegex}/\(${RecursiveRegex}\)/gsme;
+1 while $field =~ s/(\(a)(([\s\\]*?(?:\d+|(?&RecursParens))){2})(?(DEFINE)(?<RecursParens>(?>\((?>(?>[^()]+)|(?:(?=.)(?&RecursParens)|))+\))))/\($2\)/gsm;
+$field =~ s/\( /\(/gsm;
 #______________________________________________________________________save file
-$fh = new IO::File "> ".$name;
-#$fh = new IO::File "> ".$originalCwd."parsed.txt";
+$fh = new IO::File "> ".$FName.".txt";
 if (defined $fh) { print $fh $field; $fh->close; }
